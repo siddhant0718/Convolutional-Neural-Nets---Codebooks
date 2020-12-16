a0e9b9e6a228b6688471475727504485e5de3018
siddhant0718 <44208498+siddhant0718@users.noreply.github.com>
2020-12-16 20:43:56 +0100

    Conv Net- Fundamentals and Application

---
+++ Conv-Net-Application.py
@@ -0,0 +1,231 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from cnn_utils import *
+
+
+np.random.seed(1)
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Example of a picture
+index = 6
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+conv_layers = {}
+
+# GRADED FUNCTION: create_placeholders
+
+def create_placeholders(n_H0, n_W0, n_C0, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    """
+    X = tf.placeholder(tf.float32, shape=(None, n_H0, n_W0, n_C0))
+    Y = tf.placeholder(tf.float32, shape=(None, n_y))
+    
+    return X, Y
+
+
+    X, Y = create_placeholders(64, 64, 3, 6)
+    print ("X = " + str(X))
+    print ("Y = " + str(Y))
+    
+    
+# GRADED FUNCTION: initialize_parameters
+def initialize_parameters():
+    
+    W1 = tf.get_variable("W1", [4, 4, 3, 8], initializer = tf.contrib.layers.xavier_initializer(seed = 0))
+    W2 = tf.get_variable("W2", [2, 2, 8, 16], initializer = tf.contrib.layers.xavier_initializer(seed = 0))
+
+    parameters = {"W1": W1,
+                  "W2": W2}
+    
+    return parameters
+
+tf.reset_default_graph()
+with tf.Session() as sess_test:
+    parameters = initialize_parameters()
+    init = tf.global_variables_initializer()
+    sess_test.run(init)
+    print("W1[1,1,1] = \n" + str(parameters["W1"].eval()[1,1,1]))
+    print("W1.shape: " + str(parameters["W1"].shape))
+    print("\n")
+    print("W2[1,1,1] = \n" + str(parameters["W2"].eval()[1,1,1]))
+    print("W2.shape: " + str(parameters["W2"].shape))
+    
+    
+# GRADED FUNCTION: forward_propagation
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+    
+    Z1 = tf.nn.conv2d(X,W1, strides = [1,1,1,1], padding = 'SAME')
+    A1 = tf.nn.relu(Z1)
+    P1 = tf.nn.max_pool(A1, ksize = [1,8,8,1], strides = [1,8,8,1], padding = 'SAME')
+    Z2 = tf.nn.conv2d(P1,W2, strides = [1,1,1,1], padding = 'SAME')
+    A2 = tf.nn.relu(Z2)
+    P2 = tf.nn.max_pool(A2, ksize = [1,4,4,1], strides = [1,4,4,1], padding = 'SAME')
+    P2 = tf.contrib.layers.flatten(P2)
+    Z3 = tf.contrib.layers.fully_connected(P2, 6, activation_fn=None)
+
+    return Z3
+
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(Z3, {X: np.random.randn(2,64,64,3), Y: np.random.randn(2,6)})
+    print("Z3 = \n" + str(a))
+
+
+# # Comnpute Cost
+    
+# GRADED FUNCTION: compute_cost 
+
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    """
+    
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = Z3, labels = Y))
+    
+    return cost
+
+    tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    cost = compute_cost(Z3, Y)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(cost, {X: np.random.randn(4,64,64,3), Y: np.random.randn(4,6)})
+    print("cost = " + str(a))
+
+# MODEL
+    
+    
+# Exercise: Complete the function below.
+
+# The model below should:
+
+# create placeholders
+# initialize parameters
+# forward propagate
+# compute the cost
+# create an optimizer
+    
+    
+# GRADED FUNCTION: model
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
+          num_epochs = 100, minibatch_size = 64, print_cost = True):
+    """
+    Implements a three-layer ConvNet in Tensorflow
+    """
+    
+    ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
+    tf.set_random_seed(1)                             # to keep results consistent (tensorflow seed)
+    seed = 3                                          # to keep results consistent (numpy seed)
+    (m, n_H0, n_W0, n_C0) = X_train.shape             
+    n_y = Y_train.shape[1]                            
+    costs = []                                        # To keep track of the cost
+    
+    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+
+
+    parameters = initialize_parameters()
+    
+    Z3 = forward_propagation(X, parameters)
+    
+    cost = compute_cost(Z3, Y)
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+    init = tf.global_variables_initializer()
+     
+    with tf.Session() as sess:
+        
+        sess.run(init)
+        
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            minibatch_cost = 0.
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+
+                (minibatch_X, minibatch_Y) = minibatch
+                """
+                """
+                _ , temp_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+                
+                minibatch_cost += temp_cost / num_minibatches
+                
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 5 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, minibatch_cost))
+            if print_cost == True and epoch % 1 == 0:
+                costs.append(minibatch_cost)
+        
+        
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        # Calculate the correct predictions
+        predict_op = tf.argmax(Z3, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+        
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print(accuracy)
+        train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+                
+        return train_accuracy, test_accuracy, parameters
+    
+
+_, _, parameters = model(X_train, Y_train, X_test, Y_test)
+
---
+++ Conv-Net-Fundamentals.py
@@ -0,0 +1,350 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+
+np.random.seed(1)
+
+# GRADED FUNCTION: zero_pad
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. 
+    """
+    
+    X_pad = np.pad(X,((0,0),(pad,pad),(pad,pad),(0,0)),mode='constant')
+    
+    return X_pad
+
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =", x.shape)
+print ("x_pad.shape =", x_pad.shape)
+print ("x[1,1] =", x[1,1])
+print ("x_pad[1,1] =", x_pad[1,1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
+
+
+# GRADED FUNCTION: conv_single_step
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+    of the previous layer.
+    
+    """
+
+    s = a_slice_prev * W
+    Z = np.sum(s,axis=None)
+    Z = Z + float(b)  
+ 
+    return Z
+
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+
+
+# GRADED FUNCTION: conv_forward
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+    
+    """
+      
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    stride = hparameters['stride']
+    pad = hparameters['pad']
+    
+    n_H = int((n_H_prev - f + 2*pad)/stride + 1)
+    n_W = int((n_W_prev - f + 2*pad)/stride + 1)
+    
+    Z = np.zeros((m, n_H, n_W, n_C))
+    
+    A_prev_pad = zero_pad(A_prev,pad)
+    
+    for i in range(m):                               # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]                               # Select ith training example's padded activation
+        for h in range(n_H):                           # loop over vertical axis of the output volume
+            for w in range(n_W):                       # loop over horizontal axis of the output volume
+                for c in range(n_C):                   # loop over channels (= #filters) of the output volume
+                    
+                    vert_start = h*stride
+                    vert_end = vert_start + f
+                    horiz_start = w*stride
+                    horiz_end = horiz_start + f
+                    
+                    a_slice_prev = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+                    
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev,W[:,:,:,c],b[:,:,:,c])
+                                        
+    
+    assert(Z.shape == (m, n_H, n_W, n_C))
+    
+    cache = (A_prev, W, b, hparameters)
+    
+    return Z, cache
+
+np.random.seed(1)
+A_prev = np.random.randn(10,5,7,4)
+W = np.random.randn(3,3,4,8)
+b = np.random.randn(1,1,1,8)
+hparameters = {"pad" : 1,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+print("Z's mean =\n", np.mean(Z))
+print("Z[3,2,1] =\n", Z[3,2,1])
+print("cache_conv[0][1][2][3] =\n", cache_conv[0][1][2][3])
+
+
+# GRADED FUNCTION: conv_forward
+
+def pool_forward(A_prev, hparameters, mode = "max"):
+    """
+    Implements the forward pass of the pooling layer
+    
+    """
+    
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+    
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+    
+    A = np.zeros((m, n_H, n_W, n_C))              
+    
+    for i in range(m):                         # loop over the training examples
+        for h in range(n_H):                     # loop on the vertical axis of the output volume
+            for w in range(n_W):                 # loop on the horizontal axis of the output volume
+                for c in range (n_C):            # loop over the channels of the output volume
+                    
+                    vert_start = h*stride
+                    vert_end = vert_start + f
+                    horiz_start = w*stride
+                    horiz_end = horiz_start + f
+                    
+                    a_prev_slice = A_prev[i,vert_start:vert_end,horiz_start:horiz_end,c]
+                    
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_prev_slice,axis=None)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_prev_slice,axis=None)
+    
+    cache = (A_prev, hparameters)
+    
+    assert(A.shape == (m, n_H, n_W, n_C))
+    
+    return A, cache
+
+np.random.seed(1)
+A_prev = np.random.randn(2, 4, 4, 3)
+hparameters = {"stride" : 2, "f": 3}
+
+A, cache = pool_forward(A_prev, hparameters)
+print("mode = max")
+print("A =", A)
+print()
+A, cache = pool_forward(A_prev, hparameters, mode = "average")
+print("mode = average")
+print("A =", A)
+
+
+# Case 2: stride of 2
+np.random.seed(1)
+A_prev = np.random.randn(2, 5, 5, 3)
+hparameters = {"stride" : 2, "f": 3}
+
+A, cache = pool_forward(A_prev, hparameters)
+print("mode = max")
+print("A.shape = " + str(A.shape))
+print("A =\n", A)
+print()
+
+A, cache = pool_forward(A_prev, hparameters, mode = "average")
+print("mode = average")
+print("A.shape = " + str(A.shape))
+print("A =\n", A)
+
+
+def conv_backward(dZ, cache):
+    """
+    Implement the backward propagation for a convolution function
+    
+    """
+    
+    (A_prev, W, b, hparameters) = cache
+    
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+   
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    stride = hparameters['stride']
+    pad = hparameters['pad']
+    
+    (m, n_H, n_W, n_C) = dZ.shape
+    
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev))                         
+    dW = np.zeros((f, f, n_C_prev, n_C))
+    db = np.zeros((1, 1, 1, n_C))
+
+    A_prev_pad = zero_pad(A_prev, pad)
+    dA_prev_pad = zero_pad(dA_prev, pad)
+    
+    for i in range(m):                       # loop over the training examples
+        
+        a_prev_pad = A_prev_pad[i,:,:,:]
+        da_prev_pad = dA_prev_pad[i,:,:,:]
+        
+        for h in range(n_H):                   # loop over vertical axis of the output volume
+            for w in range(n_W):               # loop over horizontal axis of the output volume
+                for c in range(n_C):           # loop over the channels of the output volume
+                    
+                    vert_start = h*stride
+                    vert_end = vert_start+f
+                    horiz_start = w*stride
+                    horiz_end = horiz_start+f
+                    
+                    a_slice = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:,:,:,c] * dZ[i, h, w, c]
+                    dW[:,:,:,c] += a_slice * dZ[i, h, w, c]
+                    db[:,:,:,c] += dZ[i, h, w, c]
+                    
+        dA_prev[i, :, :, :] = da_prev_pad[pad:-pad, pad:-pad, :]
+    
+    assert(dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
+    
+    return dA_prev, dW, db
+
+
+np.random.seed(1)
+dA, dW, db = conv_backward(Z, cache_conv)
+print("dA_mean =", np.mean(dA))
+print("dW_mean =", np.mean(dW))
+print("db_mean =", np.mean(db))
+
+def create_mask_from_window(x):
+    """
+    Creates a mask from an input matrix x, to identify the max entry of x.
+    
+    """
+    
+    mask = (x==np.max(x))
+    
+    return mask
+
+
+np.random.seed(1)
+x = np.random.randn(2,3)
+mask = create_mask_from_window(x)
+print('x = ', x)
+print("mask = ", mask)
+
+def distribute_value(dz, shape):
+    """
+    Distributes the input value in the matrix of dimension shape
+    
+    """
+    
+    (n_H, n_W) = shape
+    
+    average = dz / (n_H * n_W)
+    
+    a = np.ones((n_H, n_W))*average
+    
+    return a
+
+
+a = distribute_value(2, (2,2))
+print('distributed value =', a)
+
+
+def pool_backward(dA, cache, mode = "max"):
+    """
+    Implements the backward pass of the pooling layer
+
+    """
+    
+    
+    (A_prev, hparameters) = cache
+    
+    stride = hparameters['stride']
+    f = hparameters['f']
+    
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+    m, n_H, n_W, n_C = dA.shape
+    
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev))
+    
+    for i in range(m):                       # loop over the training examples
+        
+        a_prev = A_prev[i,:,:,:]
+        
+        for h in range(n_H):                   # loop on the vertical axis
+            for w in range(n_W):               # loop on the horizontal axis
+                for c in range(n_C):           # loop over the channels (depth)
+                    
+                    vert_start = h*stride
+                    vert_end = vert_start+f
+                    horiz_start = w*stride
+                    horiz_end = horiz_start+f
+                    
+                    if mode == "max":
+                        
+                        a_prev_slice = a_prev[vert_start:vert_end,horiz_start:horiz_end,c]
+                        mask = create_mask_from_window(a_prev_slice)
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += mask * dA[i,h,w,c] 
+                        
+                    elif mode == "average":
+                        
+                        da = dA[i, h, w, c]
+                        shape = (f,f)
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += distribute_value(da, shape)
+                        
+    
+    assert(dA_prev.shape == A_prev.shape)
+    
+    return dA_prev
+
+
+np.random.seed(1)
+A_prev = np.random.randn(5, 5, 3, 2)
+hparameters = {"stride" : 1, "f": 2}
+A, cache = pool_forward(A_prev, hparameters)
+dA = np.random.randn(5, 4, 2, 2)
+
+dA_prev = pool_backward(dA, cache, mode = "max")
+print("mode = max")
+print('mean of dA = ', np.mean(dA))
+print('dA_prev[1,1] = ', dA_prev[1,1])  
+print()
+dA_prev = pool_backward(dA, cache, mode = "average")
+print("mode = average")
+print('mean of dA = ', np.mean(dA))
+print('dA_prev[1,1] = ', dA_prev[1,1]) 
+
+
